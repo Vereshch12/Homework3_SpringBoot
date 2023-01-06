6c1e58480f966fca6ee1486d59875f22361e5572
Vereshchagin_Mikhail <farasers@mail.ru>
2023-01-06 05:19:58 +0300

    ДЗ №3 Spring

---
+++ src/main/java/com/example/homework3_springboot/Homework3SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework3_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework3SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework3SpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework3_springboot/Homework3SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework3_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework3SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
